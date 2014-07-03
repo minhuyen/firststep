0ee481d47c85daec77da01e279576df0b473cdfd
dominhuyen <minhuyendo@gmail.com>
2014-07-03 23:14:31 +0700

    config mysql

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -99,15 +99,12 @@ TEMPLATE_DIRS = (
 if ON_OPENSHIFT:
      DATABASES = {
          'default': {
-             #'ENGINE': 'django.db.backends.sqlite3',
-             #'NAME': os.path.join(os.environ['OPENSHIFT_DATA_DIR'], 'db.sqlite3'),
-             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-             'NAME': 'firststep',                      # Or path to database file if using sqlite3.
-             # The following settings are not used with sqlite3:
-             'USER': 'adminmeCDSmA',
-             'PASSWORD': 'adminmeCDSmA',
-             'HOST': os.environ['OPENSHIFT_MYSQL_DB_HOST'],                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-             'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT'],                      # Set to empty string for default.
+             'ENGINE': 'django.db.backends.mysql',
+             'NAME': os.environ['OPENSHIFT_APP_NAME'],
+             'USER': os.environ['OPENSHIFT_MYSQL_DB_USERNAME'],
+             'PASSWORD': os.environ['OPENSHIFT_MYSQL_DB_PASSWORD'],
+             'HOST': os.environ['OPENSHIFT_MYSQL_DB_HOST'],
+             'PORT': os.environ['OPENSHIFT_MYSQL_DB_PORT']                    # Set to empty string for default.
          }
      }
 else:
