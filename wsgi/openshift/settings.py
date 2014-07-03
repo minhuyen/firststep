7d8e3d9d37f0500c9b4dfc89165869106aa26414
dominhuyen <minhuyendo@gmail.com>
2014-07-04 00:19:44 +0700

    config mysql

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -141,5 +141,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'static', 'media')
 MEDIA_URL = '/media/'
