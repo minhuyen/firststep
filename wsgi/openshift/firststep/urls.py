f597237ee10746270de9ea52b4bf42612b4efa94
dat <shinbutchi@ymail.com>
2014-07-04 21:39:40 +0700

    Làm trang nhà đất cần bán

--- wsgi/openshift/firststep/urls.py
+++ wsgi/openshift/firststep/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from firststep import views
 
 urlpatterns = patterns('',
-                       # url(r'^$', views.home, name='home'),
-                       # url(r'^nha-dat-can-ban/$', views.houseForSaleList, name='hfs_list'),
-                       url(r'^$', views.houseForSaleList, name='hfs_list'),
+                       url(r'^$', views.home, name='home'),
+                       url(r'^nha-dat-can-ban/$', views.houseForSaleList, name='hfs_list'),
+                       # url(r'^$', views.houseForSaleList, name='hfs_list'),
                        # url(r'^(?P<ja_id>\d+)/detail/$', views.detail, name='detail'),
                        )
