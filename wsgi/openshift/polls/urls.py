0b8ac537e271bc586c1c23b367f99b91e6f0c66b
dat <shinbutchi@ymail.com>
2014-07-07 00:03:57 +0700

    Làm trang nhà đất cần bán

--- wsgi/openshift/polls/urls.py
+++ wsgi/openshift/polls/urls.py
@@ -5,12 +5,12 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-    # ex: /polls/
+    # ex: /firststep/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /firststep/5/
     url(r'(?P<poll_id>\d+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
+    # ex: /firststep/5/results/
     url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+    # ex: /firststep/5/vote/
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
