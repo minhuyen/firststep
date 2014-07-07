82813b4b7398dfbec17c30c4f170cc6f1d5bc55e
Hanh Nguyen Ngoc <nguyenngochanh318@gmail.com>
2014-07-08 00:39:01 +0700

    settings.py conflict

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -144,3 +144,34 @@ EMAIL_HOST_USER = 'nguyenngochanh318@gmail.com'
 EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = ''
 DEFAULT_TO_EMAIL = 'nguyenngochanh318@gmail.com'
+
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+            },
+        }
+}
+
+REDACTOR_OPTIONS = {'lang': 'en',
+                    "imageUpload": "false",
+                    "linkFileUpload": "false",
+                    "fileUpload": "false",
+                    }
+REDACTOR_UPLOAD = 'uploads/'
