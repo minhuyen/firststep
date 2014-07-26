19d3c3aed6a81042fff0789944831426a5607f2a
dominhuyen <minhuyendo@gmail.com>
2014-07-26 22:42:54 +0700

    debug server

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = use_keys['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 if ON_OPENSHIFT:
      DEBUG = True
-else:True
+else:
      DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
