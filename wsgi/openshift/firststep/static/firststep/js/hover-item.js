da2ebf2480a94e89d1d834c271984571e990dddf
dominhuyen <minhuyendo@gmail.com>
2014-07-31 23:41:33 +0700

    #37 fix bug khong click footer and title hiển thị nhiều dòng trên slide home

--- wsgi/openshift/firststep/static/firststep/js/hover-item.js
+++ wsgi/openshift/firststep/static/firststep/js/hover-item.js
@@ -1,17 +1,20 @@
-$( ".left-img" ).hover(
-	function() {
-		$( this ).find( ".infor-tin" ).css('margin-left', '62%');
-		$( this ).find( ".infor-tin" ).css('width', '38%');
-	}, function() {
-		$( this ).find( ".infor-tin" ).css('margin-left', '66%');
-		$( this ).find( ".infor-tin" ).css('width', '34%');
-	}
-);
+$(document).ready(function(){
+    $( ".left-img" ).hover(
+        function() {
+            $( this ).find( ".infor-tin" ).css('margin-left', '62%');
+            $( this ).find( ".infor-tin" ).css('width', '38%');
+        }, function() {
+            $( this ).find( ".infor-tin" ).css('margin-left', '66%');
+            $( this ).find( ".infor-tin" ).css('width', '34%');
+        }
+    );
 
-$( ".right-img" ).hover(
-	function() {
-		$( this ).find( ".infor-tin" ).css('width', '38%');
-	}, function() {
-		$( this ).find( ".infor-tin" ).css('width', '34%');
-	}
-);
+    $( ".right-img" ).hover(
+        function() {
+            $( this ).find( ".infor-tin" ).css('width', '38%');
+        }, function() {
+            $( this ).find( ".infor-tin" ).css('width', '34%');
+        }
+    );
+});
+
